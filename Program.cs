b6ebe5678d01be1fc5e38ea72d171fceb1abc09b
FayronWDG <fayronwdg@gmail.com>
2025-04-30 23:06:57 +0000

    FANTATICA PRIMEIRA VERSÂO LISTA 4 '-'

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿para(int i=0; i=>0 ;){
+    switch(i){ 
+    case 1 :
+        Exercicio1.Rodar();
+    break;
+    case 2:
+        Exercicio2.Rodar();
+    break;
+    case 3:
+        Exercicio3.Rodar();
+    break;
+    }
+}
