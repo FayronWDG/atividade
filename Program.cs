de86e1dac4b95efa07f4f2f354b8bab3edc12bfb
FayronWDG <fayronwdg@gmail.com>
2025-05-02 21:26:30 +0000

    exercicio 1 feito

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
-﻿para(int i=0; i=>0 ;){
-    switch(i){ 
-    case 1 :
-        Exercicio1.Rodar();
-    break;
-    case 2:
-        Exercicio2.Rodar();
-    break;
-    case 3:
-        Exercicio3.Rodar();
-    break;
+﻿  for(int i=1; i>=1 ;){
+    Console.WriteLine("Escreva de 1 a 10, cada um vai abrir um programa diferente,para finalizar o programa digite 0.");
+    i=int.Parse(Console.ReadLine());
+        switch(i){ 
+        case 1 :
+            Ex1.Rodar();
+        break;
+        case 2 :
+            //Ex2.Rodar();
+        break;
+        case 3 :
+            //Ex3.Rodar();
+        break;
+        }
     }
-}
