3875f54cbe679dd8d84524e82970b2d3adbcbec3
FayronWDG <fayronwdg@gmail.com>
2025-05-07 22:16:06 +0000

    fanta

--- obj/Debug/net8.0/atividade.AssemblyInfo.cs
+++ obj/Debug/net8.0/atividade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atividade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ebe5678d01be1fc5e38ea72d171fceb1abc09b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de86e1dac4b95efa07f4f2f354b8bab3edc12bfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("atividade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atividade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
